c64eebb656a8da91381c2ed65d7cf1e498d49b1d
Асадбек* <asadbekme2002@gmail.com>
2022-08-07 23:17:13 +0500

    updated files

--- js/app.js
+++ js/app.js
@@ -7,6 +7,31 @@ if (menu && menuBtn) {
     menuBtn.addEventListener('click', () => {
         menu.classList.toggle('active');
         menuBtn.classList.toggle('active');
-        body.classList.toggle('_lock');
+        body.classList.toggle('__lock');
+    })
+
+    menu.querySelectorAll('.menu__link').forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('active');
+            menuBtn.classList.remove('active');
+            body.classList.remove('__lock');
+        })
     })
 }
+
+/*===============================*/ 
+const anchors = document.querySelectorAll('a[href*="#"]');
+anchors.forEach(anchor => {
+    console.log(anchor);
+
+    anchor.addEventListener('click', event => {
+        event.preventDefault();
+
+        const blockID = anchor.getAttribute('href').substring(1);
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+})
